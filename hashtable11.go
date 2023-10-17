1737fd73ac9ce99b8898236612f6569c3ecda013
FHN1V2 <parfenov200407@gmail.com>
2023-10-17 15:38:34 +0700

    hachtable

--- hashtable11.go
+++ hashtable11.go
@@ -2,110 +2,88 @@ package main
 
 import (
 	"errors"
-	"fmt"
 )
 
-type HashMap struct {
+type HashMap  struct {
 	table [512] *Pair
 }
 
 type Pair struct {
-	key   string
+	key string
 	value string
 }
 
-func HashTable(key string, size int) (int, error) {
-	if len(key) == 0 {
-		return 0, errors.New("keySize 0")
+func calcHash(key string, size int)(int, error){
+	if len(key) == 0{
+		return 0, errors.New("no value")
 	}
-	hashSum := 0
-	for i := 0; i < len(key); i++ {
-		hashSum += int(key[i])
+	hash := 0
+	for i := 0; i < len(key); i++{
+		hash += int(key[i])
 	}
-	return hashSum % size, nil
+	return hash % size,nil
 }
 
-func (hmap *HashMap) insert(key string, value string) error {
-	p := &Pair{key: key, value: value}
-	hash, err := HashTable(key, len(hmap.table))
-	if err != nil {
-		return err
+func (hmap *HashMap) Insert (key string, value string) (error){
+	p := new(Pair)
+	p.key = key
+	p.value = value
+	hash, err := calcHash(key, len(hmap.table))
+	if err != nil{
+		return errors.New("unacceptable key")
 	}
-	if hmap.table[hash] == nil {
+	if hmap.table[hash] == nil{
 		hmap.table[hash] = p
 		return nil
 	}
-	for i := (hash + 1) % len(hmap.table); i != hash; i = (i + 1) % len(hmap.table) {
-		if hmap.table[i] == nil {
+	if hmap.table[hash].key == key{
+		return errors.New("this key already exists")
+	}
+	for i := (hash + 1) % len(hmap.table); i != hash; i= (i + 1) % len(hmap.table){
+		if hmap.table[i] == nil{
 			hmap.table[i] = p
 			return nil
 		}
-	}
-	return errors.New("full")
-}
-
-func (hmap *HashMap) get(key string) (string, error) {
-	hash, err := HashTable(key, len(hmap.table))
-	if err != nil {
-		return "", err
-	}
-
-	for i := hash; hmap.table[i] != nil; i = (i + 1) % len(hmap.table) {
-		if hmap.table[i].key == key {
-			return hmap.table[i].value, errors.New("")
+		if hmap.table[hash].key == key{
+			return errors.New("this key already exists")
 		}
 	}
-
-	return "", errors.New("not found")
+	return errors.New("table is full")
 }
 
-func (hmap *HashMap) del(key string) error {
-	hash, err := HashTable(key, len(hmap.table))
-	if err != nil {
-		return err
+func (hmap *HashMap) Get (key string) (string, error){
+	hash, err := calcHash(key, len(hmap.table))
+	if err != nil{
+		return "", errors.New("unacceptable key")
 	}
+	if hmap.table[hash] != nil && hmap.table[hash].key == key{
+		return hmap.table[hash].value, nil
+	}
+	for i := (hash + 1) % len(hmap.table); i != hash; i = (i + 1) % len(hmap.table){
+		if hmap.table[i] != nil && hmap.table[i].key == key{
+			return hmap.table[i].value, nil
+		}
+	}
+	return "", errors.New("no such key")
+}
 
-	for i := hash; hmap.table[i] != nil; i = (i + 1) % len(hmap.table) {
-		if hmap.table[i].key == key { 	
+func (hmap *HashMap) Del (key string) (error){
+	hash, err := calcHash(key, len(hmap.table))
+	if err != nil{
+		return errors.New("unacceptable key")
+	}
+	if hmap.table[hash] == nil{
+		return errors.New("nothing to delete")
+	}
+	if hmap.table[hash] != nil && hmap.table[hash].key == key{
+		hmap.table[hash] = nil
+		return nil
+	}
+	for i := hash + 1; i != hash; i= (i + 1) % len(hmap.table){
+		if hmap.table[i] != nil && hmap.table[i].key == key{
 			hmap.table[i] = nil
 			return nil
 		}
 	}
-
-	return errors.New("not found")
-}
-
-
-
-func main() {
-	// Create a new HashMap
-	hmap := &HashMap{}
-
-	// Insert some key-value pairs
-	hmap.insert("2", "value1")
-	hmap.insert("1", "value2")
-	hmap.insert("3", "value3")
-
-	// Retrieve values by keys
-	val, err := hmap.get("1")
- 	if err == nil {
-		fmt.Printf("1: %s\n", val)
-	} else {
-		fmt.Println("key1 not found")
-	}
-
-	val, err = hmap.get("key2")
-	if err == nil {
-		fmt.Printf("key2: %s\n", val)
-	} else {
-		fmt.Println("key2 not found")
-	}
-
-	// Delete a key-value pair
-	err = hmap.del("key2")
-	if err == nil {
-		fmt.Println("key2 deleted")
-	} else {
-		fmt.Println("key2 not found, couldn't delete")
-	}
-}
+	return errors.New("no such key")
+} 
